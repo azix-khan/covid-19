a1ec3da1ef9f2656e863f38bf2c447c7823b1c1e
azix-khan <azixkhan.55@gmail.com>
2023-07-28 23:27:32 +0500

    showing json data

--- lib/View/countries_list.dart
+++ lib/View/countries_list.dart
@@ -1,3 +1,4 @@
+import 'package:covid_tracker/Services/states_services.dart';
 import 'package:flutter/material.dart';
 
 class CountriesListScreen extends StatefulWidget {
@@ -8,8 +9,45 @@ class CountriesListScreen extends StatefulWidget {
 }
 
 class _CountriesListScreenState extends State<CountriesListScreen> {
+  StatesServices statesServices = StatesServices();
+  TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              controller: searchController,
+              decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                  hintText: 'Search with country name',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0))),
+            ),
+          ),
+          Expanded(
+              child: FutureBuilder(
+                  future: statesServices.countriesListApi(),
+                  builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Text('Loading');
+                    } else {
+                      return ListView.builder(itemBuilder: (context, index) {
+                        return const Column(
+                          children: [],
+                        );
+                      });
+                    }
+                  }))
+        ],
+      )),
+    );
   }
 }
